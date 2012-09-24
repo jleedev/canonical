e0db538610bef423b8924d72371c51bdfad6205a
Josh Lee <jleedev@gmail.com>
2012-09-24 16:20:00 -0400

    Changed the message passing

--- background.js
+++ background.js
@@ -1,24 +1,17 @@
-var responses = [];
-
 function sendVisitCanonical(tab) {
-  if (tab.id in responses) {
-    var message = {type: VISIT_CANONICAL};
-    responses[tab.id](message);
-    delete responses[tab.id];
-  }
+  var message = {type: VISIT_CANONICAL};
+  chrome.tabs.sendMessage(tab.id, message);
 }
 
 chrome.pageAction.onClicked.addListener(sendVisitCanonical);
 
-function updateVisibilityHandler(message, sender, responseCallback) {
+function updateVisibilityHandler(message, sender) {
   var tab = sender.tab;
   if (message.type != UPDATE_VISIBILITY) return;
   var title = "Canonical URL: " + message.url;
   if (message.visibility) {
     chrome.pageAction.show(tab.id);
     chrome.pageAction.setTitle({tabId: tab.id, title: title});
-    responses[tab.id] = responseCallback;
-    return true;
   } else {
     chrome.pageAction.hide(tab.id);
   }
--- content.js
+++ content.js
@@ -1,5 +1,7 @@
-function visitCanonical(response, canonicalUrl) {
-  if (response.type != VISIT_CANONICAL) return;
+var canonicalUrl;
+
+function visitCanonical(message, sender) {
+  if (message.type != VISIT_CANONICAL) return;
   // Try it seamlessly, but that fails if the origin's going to change
   try {
     history.replaceState(undefined, '', canonicalUrl)
@@ -10,20 +12,19 @@ function visitCanonical(response, canonicalUrl) {
   updateVisibility();
 }
 
+chrome.extension.onMessage.addListener(visitCanonical);
+
 function updateVisibility() {
   var canonicalLink = document.querySelector('link[rel="canonical"]');
   if (canonicalLink == null) return;
   // nb: will be absolute
-  var canonicalUrl = canonicalLink.href;
+  canonicalUrl = canonicalLink.href;
   message = {
     type: UPDATE_VISIBILITY,
     visibility: true,
     url: canonicalUrl,
   };
-  var responseCallback = function(response) {
-    return visitCanonical(response, canonicalUrl);
-  }
-  chrome.extension.sendMessage(undefined, message, responseCallback);
+  chrome.extension.sendMessage(undefined, message);
 }
 
 updateVisibility();
